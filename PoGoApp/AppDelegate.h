33cf33407373bd3b1bc2d75de4c542363bfc37d3
Xiaonan Wang <xiaonaw@amazon.com>
2016-07-14 15:12:00 -0700

    Initial Commit

---
+++ PoGoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PoGoApp
+//
+//  Created by Wang, Xiaonan on 7/14/16.
+//  Copyright © 2016 Wang, Xiaonan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PoGoApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PoGoApp
+//
+//  Created by Wang, Xiaonan on 7/14/16.
+//  Copyright © 2016 Wang, Xiaonan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
